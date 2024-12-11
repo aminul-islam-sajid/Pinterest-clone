585a3363643f81aad95d24dc5636c88c5afb750d
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-11 10:05:21 +0600

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
