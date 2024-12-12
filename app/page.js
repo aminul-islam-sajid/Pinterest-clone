ecb7fd7fda81dcdad7f04a834a575ab177b80490
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-12 13:11:16 +0600

    img add

--- app/page.js
+++ app/page.js
@@ -1,16 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ClipLoader } from "react-spinners";
 
 export default function Home() {
   const gallery = [
     {
       _id: 1,
       title: "spider man",
-      imageURL: ""
+      imageURL: "/img1.jpeg"
+    },
+    {
+      _id: 2,
+      title: "spider man",
+      imageURL: "/img4.jpeg"
+    },
+    {
+      _id: 3,
+      title: "spider man",
+      imageURL: "/img3.jpeg"
     }
   ]
   return (
     <>
       <div className="container mx-auto p-4">
+        {
+          (!gallery || gallery.length <= 0) ? (<div className="flex justify-center items-center min-h-[750pxs]">
+            <ClipLoader color="#ef4444" size={120} />
+          </div>
+          ) : gallery.length > 0 ? (
+            <div>
+              {
+                gallery.map(item => {
+                  return (
+                    <Link href={`/pin/${item._id}`} key={item._id} className="relative mb-4">
+                      <Image src={item.imageURL} alt={item.title} height={300} width={300} className="h-auto w-full rounded-lg" />
+                    </Link>
+                  )
+                })
+              }
+            </div>) : ("")
 
+        }
       </div>
     </>
   );
