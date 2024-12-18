76c61a94c2014316956e5dc9cd38ef68cc9f3ad1
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-18 13:26:58 +0600

    fix my site

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,24 +1,21 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
-import GoogleProvider from "next-auth/providers/google"
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 import connectToDb from "@/pinterest/libs/mongodb";
-import user from "@/pinterest/models/user";
-import bcrypt from "bcrypt"
-import { User } from "lucide-react";
-import { pages } from "next/dist/build/templates/app-page";
-import { signIn } from "next-auth/react";
+import User from "@/pinterest/models/user";
+import bcrypt from "bcrypt";
 
 const authOption = {
-    provider: [
+    providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLINT_ID,
-            clientSecret: process.env.GOOGLE_CLINT_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
         GithubProvider({
-            clientId: process.env.GITHUB_CLINT_ID,
-            clientSecret: process.env.GITHUB_CLINT_SECRET
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
         CredentialsProvider({
             name: "Credentials",
@@ -26,40 +23,38 @@ const authOption = {
                 username: {
                     label: "Username",
                     type: "text",
-                    placeholder: "Enter your Username"
-
+                    placeholder: "Enter your Username",
                 },
                 password: {
                     label: "Password",
                     type: "password",
-                    placeholder: "Enter your password"
-
+                    placeholder: "Enter your password",
                 },
             },
             async authorize(credentials) {
-                connectToDb()
+                await connectToDb();
                 const user = await User.findOne({ username: credentials.username });
                 if (!user) {
                     console.log('User not found');
-                    return null
+                    return null;
                 }
-                const isPasswordMatched = await bcrypt.compare(credentials.password, user.password)
+                const isPasswordMatched = await bcrypt.compare(credentials.password, user.password);
                 if (!isPasswordMatched) {
-                    return null
+                    console.log('Invalid password');
+                    return null;
                 }
                 return {
                     name: user.name,
                     email: user.email,
-                    image: user.image
-                }
-            }
-        })
+                    image: user.image,
+                };
+            },
+        }),
     ],
-
     pages: {
         signIn: "/signin",
     },
-}
-const handler = NextAuth(authOption)
+};
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOption);
+export { handler as GET, handler as POST };
