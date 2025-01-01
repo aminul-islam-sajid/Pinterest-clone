6bf126c7d9f40ba4a4edd3a923afe99512b0da3b
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2025-01-01 15:57:41 +0600

    register route setup

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -3,6 +3,7 @@ import connectToDb from "@/pinterest/libs/mongodb";
 import User from "@/pinterest/models/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import { resolve } from "styled-jsx/css";
 
 export async function POST(request) {
     connectToDb();
@@ -14,6 +15,39 @@ export async function POST(request) {
     const password = fromData.get("password")
 
     if (!image) {
-        return NextResponse.json()
+        return NextResponse.json({ error: "no file received" }, { status: 400 })
     }
+
+    try {
+        const arrayBuffer = await image.arrayBuffer();
+        const buffer = new Uint8Array(arrayBuffer)
+
+        const uploadedResponse = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({}, function (error, result) {
+                if (error) {
+                    reject(error)
+                    return
+                }
+                resolve(result)
+            }).end(buffer)
+        })
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+            image: uploadedResponse.secure_url
+        })
+        return NextResponse.json({
+            success: true,
+            massage: "user registered",
+            user
+        }, { status: 201 })
+    } catch (error) {
+        console.error("user registration Failed", error);
+    }
+    return NextResponse.json({
+        error: "user registration failed"
+    }, { status: 500 })
 }
